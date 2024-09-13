5eedaaf69597983c38b2cecfae623f8b086d4b47
Crunkle <justcrunkle@hotmail.co.uk>
2024-09-13 15:21:16 +0100

    Add haptic feedback patches

--- include/main.hpp
+++ include/main.hpp
@@ -7,18 +7,15 @@
 // beatsaber-hook is a modding framework that lets us call functions and fetch
 // field values from in the game It also allows creating objects, configuration,
 // and importantly, hooking methods to modify their values
-#include "beatsaber-hook/shared/config/config-utils.hpp"
 #include "beatsaber-hook/shared/utils/hooking.hpp"
 #include "beatsaber-hook/shared/utils/il2cpp-functions.hpp"
-#include "beatsaber-hook/shared/utils/logging.hpp"
-
 
 #include "paper/shared/logger.hpp"
 
-#include "_config.hpp"
+#include "config-utils/shared/config-utils.hpp"
+#include "bsml/shared/BSML.hpp"
 
-// Define these functions here so that we can easily read configuration and
-// log information from other files
-Configuration &getConfig();
+#define MOD_EXPORT __attribute__((visibility("default")))
+#define MOD_EXTERN_FUNC extern "C" MOD_EXPORT
 
 constexpr auto PaperLogger = Paper::ConstLoggerContext("BetterHaptics");
--- src/main.cpp
+++ src/main.cpp
@@ -1,36 +1,136 @@
 #include "main.hpp"
 
-#include "scotland2/shared/modloader.h"
+#include "GlobalNamespace/NoteCutCoreEffectsSpawner.hpp"
+#include "GlobalNamespace/NoteCutInfo.hpp"
+#include "GlobalNamespace/NoteCutHapticEffect.hpp"
+
+#include "GlobalNamespace/OculusAdvancedHapticFeedbackPlayer.hpp"
 
 static modloader::ModInfo modInfo{MOD_ID, VERSION, 0};
 // Stores the ID and version of our mod, and is sent to
 // the modloader upon startup
 
-// Loads the config from disk using our modInfo, then returns it for use
-// other config tools such as config-utils don't use this config, so it can be
-// removed if those are in use
-Configuration &getConfig() {
-  static Configuration config(modInfo);
-  return config;
+DECLARE_CONFIG(
+    BetterHapticsConfig,
+    CONFIG_VALUE(globalBypass, bool, "globalBypass", false);
+    CONFIG_VALUE(separateArcs, bool, "separateArcs", true);
+);
+
+bool get_global_bypass() {
+    return getBetterHapticsConfig().globalBypass.GetValue();
+}
+
+void set_global_bypass(bool globalBypass) {
+    getBetterHapticsConfig().globalBypass.SetValue(globalBypass);
+}
+
+bool get_separate_arcs() {
+    return getBetterHapticsConfig().separateArcs.GetValue();
+}
+
+void set_separate_arcs(bool separateArcs) {
+    getBetterHapticsConfig().separateArcs.SetValue(separateArcs);
 }
 
 // Called at the early stages of game loading
 MOD_EXTERN_FUNC void setup(CModInfo *info) noexcept {
-  *info = modInfo.to_c();
+    *info = modInfo.to_c();
 
-  getConfig().Load();
+    // File logging
+    Paper::Logger::RegisterFileContextId(PaperLogger.tag);
 
-  // File logging
-  Paper::Logger::RegisterFileContextId(PaperLogger.tag);
+    PaperLogger.info("Completed setup!");
+}
 
-  PaperLogger.info("Completed setup!");
+MAKE_HOOK_MATCH(
+    NoteCutCoreEffectsSpawner_PlayHitNoteHapticEffect,
+    &GlobalNamespace::NoteCutCoreEffectsSpawner::PlayHitNoteHapticEffect, void,
+    GlobalNamespace::NoteCutCoreEffectsSpawner *self, GlobalNamespace::NoteCutInfo noteCutInfo, GlobalNamespace::NoteData *noteData
+) {
+    if (get_global_bypass() || get_separate_arcs()) {
+        NoteCutCoreEffectsSpawner_PlayHitNoteHapticEffect(self, noteCutInfo, noteData);
+        return;
+    }
+
+    if (!noteCutInfo.allIsOK) {
+        self->_noteCutHapticEffect->HitNote(noteCutInfo.saberType, GlobalNamespace::NoteCutHapticEffect::Type::BadCut);
+    } else {
+        self->_noteCutHapticEffect->HitNote(noteCutInfo.saberType, GlobalNamespace::NoteCutHapticEffect::Type::Normal);
+    }
+}
+
+MAKE_HOOK_MATCH(
+    NoteCutCoreEffectsSpawner_PlayHitChainNoteHapticEffect,
+    &GlobalNamespace::NoteCutCoreEffectsSpawner::PlayHitChainNoteHapticEffect, void,
+    GlobalNamespace::NoteCutCoreEffectsSpawner *self, GlobalNamespace::NoteCutInfo noteCutInfo, bool isChainHead
+) {
+    if (get_global_bypass()) {
+        NoteCutCoreEffectsSpawner_PlayHitChainNoteHapticEffect(self, noteCutInfo, isChainHead);
+        return;
+    }
+
+    if (!noteCutInfo.allIsOK) {
+        self->_noteCutHapticEffect->HitNote(noteCutInfo.saberType, GlobalNamespace::NoteCutHapticEffect::Type::BadCut);
+    } else if (isChainHead) {
+        self->_noteCutHapticEffect->HitNote(noteCutInfo.saberType, GlobalNamespace::NoteCutHapticEffect::Type::ChainHead);
+    } else {
+        self->_noteCutHapticEffect->HitNote(noteCutInfo.saberType, GlobalNamespace::NoteCutHapticEffect::Type::ChainLink);
+    }
+}
+
+MAKE_HOOK_MATCH(
+    OculusAdvancedHapticFeedbackPlayer_CanPlayHapticPreset,
+    &GlobalNamespace::OculusAdvancedHapticFeedbackPlayer::CanPlayHapticPreset, bool,
+    GlobalNamespace::OculusAdvancedHapticFeedbackPlayer *self, Libraries::HM::HMLib::VR::HapticPresetSO *hapticPreset, UnityEngine::XR::XRNode node
+) {
+    if (get_global_bypass()) {
+        return OculusAdvancedHapticFeedbackPlayer_CanPlayHapticPreset(self, hapticPreset, node);
+    }
+
+    return false;
+}
+
+float calculate_offset_y(UnityEngine::Component *component) {
+    UnityW<UnityEngine::Transform> transform = component->transform;
+    UnityW<UnityEngine::RectTransform> rectTransform = transform.cast<UnityEngine::RectTransform>();
+    UnityEngine::Rect rect = rectTransform->rect;
+
+    return -rect.m_Height;
+}
+
+void menu_controller(HMUI::ViewController *self, bool firstActivation, bool addedToHierarchy, bool screenSystemEnabling) {
+    if (!firstActivation) {
+        return;
+    }
+
+    float offsetY = 0.0f;
+
+    BSML::ToggleSetting *globalBypassToggle = BSML::Lite::CreateToggle(self->transform, "Global Bypass", get_global_bypass(), {0.0f, offsetY}, set_global_bypass);
+    offsetY += calculate_offset_y(globalBypassToggle);
+
+    BSML::ToggleSetting *separateArcsToggle = BSML::Lite::CreateToggle(self->transform, "Separate Arcs", get_separate_arcs(), {0.0f, offsetY}, set_separate_arcs);
+    offsetY += calculate_offset_y(separateArcsToggle);
 }
 
 // Called later on in the game loading - a good time to install function hooks
 MOD_EXTERN_FUNC void late_load() noexcept {
-  il2cpp_functions::Init();
+    il2cpp_functions::Init();
 
-  PaperLogger.info("Installing hooks...");
+    getBetterHapticsConfig().Init(modInfo);
+    BSML::Init();
 
-  PaperLogger.info("Installed all hooks!");
+    BSML::Register::RegisterMainMenuViewControllerMethod(
+        "BetterHaptics by Crunkle",
+        "BetterHaptics",
+        "",
+        menu_controller
+    );
+
+    PaperLogger.info("Installing hooks...");
+
+    INSTALL_HOOK(PaperLogger, NoteCutCoreEffectsSpawner_PlayHitNoteHapticEffect);
+    INSTALL_HOOK(PaperLogger, NoteCutCoreEffectsSpawner_PlayHitChainNoteHapticEffect);
+    INSTALL_HOOK(PaperLogger, OculusAdvancedHapticFeedbackPlayer_CanPlayHapticPreset);
+
+    PaperLogger.info("Installed all hooks!");
 }
